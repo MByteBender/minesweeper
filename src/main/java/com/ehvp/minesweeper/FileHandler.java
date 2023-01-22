453bffa6eaf9e99002815e0d7c07e18cf4e8a49f
tristan <tristanwestreicher@gmail.com>
2023-01-22 03:01:10 +0100

    fixed highscore room Style + changed package name

---
+++ src/main/java/com/ehvp/minesweeper/FileHandler.java
@@ -0,0 +1,65 @@
+package com.ehvp.minesweeper;
+
+import java.io.*;
+import java.util.Scanner;
+
+/**
+ * handles the highscore writting/ reading/ creating
+ */
+
+public class FileHandler {
+
+
+    /**
+     * creates a highscore-file in the resources directory and handels errors
+     * checks also if file already exists
+     */
+    public static void createFile() {
+        try {
+            File myObj = new File("src/main/resources/highscore.dat");
+            if (myObj.createNewFile()) {
+                writeToFile(0);
+                System.out.println("File created: " + myObj.getName());
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * writes the highscore in the file and converts the score to a String to write it correctly to the file
+     */
+    public static void writeToFile(int score) {
+        Integer scoreWrapped = score;
+        try {
+            FileWriter myWriter = new FileWriter("src/main/resources/highscore.dat");
+            myWriter.write(scoreWrapped.toString());
+            myWriter.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * reads the score out of the highscore.dat file and handels an exception when the file is not existend
+     */
+    public static String readFile() {
+        String data = "";
+        try {
+            File myObj = new File("src/main/resources/highscore.dat");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                data = myReader.nextLine();
+            }
+            myReader.close();
+            return data;
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+            return "";
+        }
+
+    }
+}
